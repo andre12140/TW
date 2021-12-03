f6308855670300b33c6152c2a4f0de24f286a6bf
André Barbosa <48558708+andre12140@users.noreply.github.com>
2021-12-03 15:14:35 +0000

    Update workerTest.js

--- Planeador Horas Destinadas/workerTest.js
+++ Planeador Horas Destinadas/workerTest.js
@@ -56,6 +56,7 @@ onmessage = function(event) {
   }
   if(started == 0){
     started = 1;
+    console.log((waitTime - (60000 * 1))
     if(waitTime - (60000 * 1) > 0){
         console.log('Going to sleep but waking up 1 min before due date');
         setTimeout(function(){
