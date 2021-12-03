15b2aeeb8bf0f1d865a73de9f6b1d8697ec71fc9
André Barbosa <48558708+andre12140@users.noreply.github.com>
2021-12-03 15:06:07 +0000

    Update workerTest.js

--- Planeador Horas Destinadas/workerTest.js
+++ Planeador Horas Destinadas/workerTest.js
@@ -1,19 +1,70 @@
-onmessage = function(event) {
-  console.log('From Main: ' + event.data);
-  console.log('Worker Recived Message');
-  postMessage('Worker Message');
-  //onCancel(); // causes "onCancel is not defined" error
+// postMessage; 0 -> Send Attack; 1 -> get Waiting Time;
+
+var waitTime;
+var started = 0;
+
+function accuTime(timer, max, callbackArgument){
+  var counter = 1;
+
+  var init = (t) => {
+    let timeStart = new Date().getTime();
+    setTimeout(function () {
+      if (counter < max) {
+        let fix = (new Date().getTime() - timeStart) - timer;
+        init(t - fix);
+        counter++;
+      } else {
+      // event to be executed at animation end
+        callbackArgument();
+      }
+    }, t);
+  }
+init(timer);
+}
+
+function getWaitTime(){
+  postMessage(1);
+}
+
+function accurateWait(){
+    console.log('Sending Attack soon');
+
+    //Fazer o calculo para saber quantos milisegundos por amostra
+    var iterations = 3000;
+    var time = waitTime;//getWaitTime();
+
+    var divInt = Math.floor(time/iterations);
+    var divRemainder = (time/iterations) % 1;
+
+    accuTime( divInt, iterations, () => {
+    setTimeout(function(){
+        postMessage(0);
+        return
+    },Math.round(divRemainder*iterations));});
 }
 
 
-/*self.onmessage = function(e) {
-    console.log(e.data);
-    self.postMessage('Worker is sending this message');
-};
 
-console.log('Starting Worker');
+onmessage = function(event) {
+  
+  console.log('From Main: ' + event.data);
+  
+  if(event.data[0] == 1){
+    waitTime = event.data[1];
+  }
+  if(started == 0){
+    started = 1;
+    if(waitTime > 0){
+        console.log('Going to sleep but waking up 1 min before due date');
+        setTimeout(function(){
+            accurateWait();
+        }, waitTime);
+    } else {
+        accurateWait();
+    }
+    
+  }
+}
 
-var timeStart = new Date();
-setTimeout(function(){
-    console.log('Worker: ' + Date.now() - timeStart)
-}, 60000);*/
+postMessage(1);
+
